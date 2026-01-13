db2380a2086c208155e36a113757faa65013deed
Tae <taehaenschke@gmail.com>
2026-01-13 06:40:55 +0100

    Let's go!
    This should be the first commit. Hopefully it works?!

---
+++ Assets/TheraBytes/BetterUI_Examples/ResolutionTracking/SetDpiText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable 0649 // never assigned warning
+
+namespace TheraBytes.BetterUi.Examples.ResolutionTracking
+{
+    public class SetDpiText : MonoBehaviour, IResolutionDependency
+    {
+        [SerializeField] Text screenDpiValueLabel;
+        [SerializeField] Text betterUiDpiValueLabel;
+        [SerializeField] Text resolutionValueLabel;
+
+
+        void Start()
+        {
+            OnResolutionChanged();
+        }
+
+        public void OnResolutionChanged()
+        {
+            screenDpiValueLabel.text = Screen.dpi.ToString();
+            betterUiDpiValueLabel.text = ResolutionMonitor.CurrentDpi.ToString();
+            resolutionValueLabel.text = $"{Screen.width} x {Screen.height}";
+        }
+
+    }
+}
+
+#pragma warning restore 0649 // never assigned warning
